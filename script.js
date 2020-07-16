e9f2de701c7a8f9c1cb2874fb168f61d2f683c01
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-07-16 12:41:46 -0700

    adding

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var viewScore = document.getElementById("view");
 var goBack = document.getElementById("go-back");
 var questionContainer = document.getElementById("question-container");
 var submitBtn = document.getElementById("submit");
+var result = document.getElementById("result");
+var userInput = "";
 //var inputBtn = document.getElementById("input");
 var secondsLeft = 75;
 var counter = 0;
@@ -47,10 +49,10 @@ var setIntervalId;
 myListner.addEventListener("click", function () {
     //document.getElementById("score").classList.add("hide");
     document.getElementById("header").classList.add("hide");
-    document.getElementById("question-container").style.display=("block");
+    document.getElementById("question-container").style.display = ("block");
     setIntervalId = setInterval(countDown, 1000)
-    myListner.style.display ="none"
-   console.log("curr:", currentQuestionIndex, setIntervalId)
+    myListner.style.display = "none"
+    console.log("curr:", currentQuestionIndex, setIntervalId)
     showquestions()
 })
 function countDown() {
@@ -64,13 +66,14 @@ function showResults() {
     localStorage.setItem("highScore", secondsLeft)
     highScore = localStorage.getItem('highScore');
     viewScore.innerHTML = highScore;
+    viewScore.classList.add("hide");
     goBack.classList.add("hide");
     resetScore.classList.add("hide");
     document.getElementById("btn").style.display = "block"
     document.getElementById("question-container").style.display = "none"
 }
 function showquestions() {
-console.log("shwQ:", currentQuestionIndex)
+    console.log("show:", currentQuestionIndex)
     document.getElementById("questions").innerHTML = questions[currentQuestionIndex].title;
     var choices = questions[currentQuestionIndex].choices
     document.getElementById("choices").textContent = ""
@@ -107,33 +110,44 @@ console.log("shwQ:", currentQuestionIndex)
 console.log("We are strating")
 
 submitBtn.addEventListener("click", function (event) {
-   
+
     // capture the data you want to send to local storage and console.log it
     // send it with localStorage.setItem(...)
     goBack.classList.remove("hide");
     resetScore.classList.remove("hide");
     resetIntial.classList.add("hide");
     submitBtn.classList.add("hide");
+    viewScore.classList.remove("hide");
 
+    function myFunction() {
+        localStorage.getItem("initial");
+        userInput.innerHTML = highScore;
+    }
+
+    /* if(event.target.matches(scoreInitial)){
+         result.textContent = userInput;
+         result.append(viewScore);
+     }*/
 });
-    
+
 goBack.addEventListener("click", function () {
     document.getElementById("header").classList.remove("hide");
     //myListner.classList.remove("hide");
-    myListner.style.display = "block"
-    resetScore.classList.add("hide");
-    goBack.classList.add("hide");
-   showResults() 
-     secondsLeft = 75;
+    myListner.style.display = "block";
+    timer.classList.remove("hide");
+    showResults()
+    clearInterval(setIntervalId)
+    secondsLeft = 75;
     document.getElementById("timer").innerHTML = secondsLeft
     setIntervalId = setInterval(countDown, 1000)
     // re init all the valuews 
+    resetScore.classList.add("hide");
+    goBack.classList.add("hide");
     currentQuestionIndex = 0
-   // showquestions()
+    // showquestions()
     localStorage.clear(highScore);
 })
-resetScore.addEventListener("click", function() {
-viewScore.classList.add("hide");
-timer.classList.add("hide");
+resetScore.addEventListener("click", function () {
+    viewScore.classList.add("hide");
+    timer.classList.add("hide");
 })
- 
